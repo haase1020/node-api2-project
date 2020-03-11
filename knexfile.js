a764688291caf46211df61f3675e963c0ffcf7f7
Mandi Haase <haase1020@gmail.com>
2020-03-10 23:40:23 -0400

    input second post in posts-router.js

--- knexfile.js
+++ knexfile.js
@@ -16,3 +16,5 @@ module.exports = {
     },
   },
 };
+
+//nothing to do
--- posts/posts-router.js
+++ posts/posts-router.js
@@ -7,6 +7,54 @@ const router = express.Router();
 
 //add post, post, get, get, get, delete, put requests
 
+router.post('/', (req,res) => {
+    const post = req.body;
+    if (!post.title|| !post.contents) {
+        res.status(400).json(
+            {errorMessage: "Please provide title and contents for the post." }
+        );
+    }else {
+        Posts.insert(post)
+        .then(post => {
+            res.status(201).json(post);
+        })
+        .catch(err =>{
+            console.log(`Error: ${err}`);
+            res.status(500).json({
+                error: 'There was an error while saving the post to the database'
+            });
+        });
+    }
+});
+
+router.post('/:id/comments', (req,res)=> {
+    const { id }=req.params;
+    const comment = {...req.body, post_id: id };
+    if (!id) {
+        res.status(404).json({
+            message: 'The post with the specified ID does not exist'
+        });
+    } else if (!req.body.text) {
+        res.status(400).json({
+            errorMessage: 'Please provide text for the comment'
+        });
+    } else {
+        Posts.insertComment(comment)
+        .then(comment => {
+            res.status(201).json(comment);
+        })
+        .catch(err => {
+            console.log(`Error:${err}`);
+            res.status(500).json({
+                error: 'There was an error while saving comment' 
+            });
+        });
+    }
+});
+
+
+
+
 router.get('/', (req, res) => {
 
 })
