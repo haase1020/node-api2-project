4730a095f350d66100719c18b6c53ffd325c5ed8
Mandi Haase <haase1020@gmail.com>
2020-03-11 01:10:57 -0400

    tried to do stretch by creating react app but axios call won't go through even after installing cors

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App =() => {
+
+  useEffect(() => {
+axios.get(`http://localhost:5000/api/posts`)
+.then(res => {
+  console.log("API response", res)
+})
+
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>POSTS API</h1>
     </div>
   );
 }
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const server = require('./api/server.js');
+const cors = require('cors');
+
+server.use(cors())
 
 const PORT = 5000;
 
